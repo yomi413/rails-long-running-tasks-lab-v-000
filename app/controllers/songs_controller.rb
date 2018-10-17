c8a9c6b253bc77599a7e2693759d3ea7c603644f
Yomaira Escano <yjescano@gmail.com>
2018-10-17 22:06:17 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,17 +1,17 @@
 class SongsController < ApplicationController
   require 'csv'
-  
+
   def index
     @songs = Song.all
   end
 
   def upload
-    
+
     CSV.foreach(params[:songs].path, headers: true) do |song|
       Song.create(song_params)
     end
     redirect_to songs_path
-    
+
   end
 
   def show
@@ -61,4 +61,3 @@ class SongsController < ApplicationController
     params.require(:song).permit(:title, :artist_name)
   end
 end
-
